a94a1f860984f4a9dd071f41331f7d9faa91d6b1
TTisalamb <ttisalamb@outlook.com>
2025-05-08 15:30:25 +0100

    change6

--- Scripts/data_analysis_M3.py
+++ Scripts/data_analysis_M3.py
@@ -25,28 +25,33 @@ def generate_means_sequence(collated_path):
  
 print(generate_means_sequence("Output/collated_answers.txt"))
 
-plt.figure(figsize=(12, 6))
-plt.scatter(range(1, 101), generate_means_sequence("Output/collated_answers.txt"), color='blue', alpha=0.7)
-plt.title('Mean Answer Values per Question')
-plt.xlabel('Question Number')
-plt.ylabel('Mean Answer Value (1-4)')
-plt.xticks(range(0, 101, 10))
-plt.yticks(range(1, 5))
-plt.grid(True, linestyle='--', alpha=0.5)
-plt.show()
 
+def visualize_data(collated_answers_path, n):
+    respondents = [file_content[i:i+100] for i in range(0, len(file_content), 100)]
 
-# Plot all respondents' answer sequences
-plt.figure(figsize=(12, 6))
-for i, seq in enumerate(file_content):  # 遍历每个回答者的序列
-    plt.plot(range(1, 101), seq,         # x轴:1-100题，y轴:答案值
-            alpha=0.3,                  # 设置透明度
-            linewidth=1)                # 线宽
-    
-plt.title('All Respondents\' Answer Sequences')
-plt.xlabel('Question Number')
-plt.ylabel('Answer Value (0=unanswered, 1-4=options)')
-plt.xticks(range(0, 101, 10))          # x轴刻度每10题一个
-plt.yticks(range(0, 5))                # y轴刻度0-4
-plt.grid(True, linestyle='--', alpha=0.5)
-plt.show()
+    if n == 1:
+        plt.figure(figsize=(12, 6))
+        plt.scatter(range(1, 101), generate_means_sequence(collated_answers_path), color='blue', alpha=0.7)
+        plt.title('Mean Answer Values per Question')
+        plt.xlabel('Question Number')
+        plt.ylabel('Mean Answer Value (1-4)')
+        plt.xticks(range(0, 101, 10))
+        plt.yticks(range(1, 5))
+        plt.grid(True, linestyle='--', alpha=0.5)
+        plt.show()
+
+    if n == 2:
+        # Plot all respondents' answer sequences
+        plt.figure(figsize=(12, 6))
+        for i, seq in enumerate(respondents):  # 遍历每个回答者的序列
+            plt.plot(range(1, 101), seq, alpha=0.3,linewidth=1)               
+        plt.title('All Respondents\' Answer line Graphs')
+        plt.xlabel('Question Number')
+        plt.ylabel('Answer Value (0=unanswered, 1-4=options)')
+        plt.xticks(range(0, 101, 10))          # x轴刻度每10题一个
+        plt.yticks(range(0, 5))                # y轴刻度0-4
+        plt.grid(True, linestyle='--', alpha=0.5)
+        plt.show()
+
+    else:
+        print("Invalid value for n. Use 1 (scatter) or 2 (line).")
